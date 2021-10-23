bd917f5115ad31d59ae585c36a37400f8eb3196c
SaltineChips <rubixblockchain@gmail.com>
2021-10-23 06:53:02 -0700

    Commit 1.1.7.2
    -Raises checks to a higher block to confirm issue source

--- Konjungate.pro
+++ Konjungate.pro
@@ -1,6 +1,6 @@
 TEMPLATE = app
 TARGET = Konjungate-qt
-VERSION = 1.1.7.1
+VERSION = 1.1.7.2
 INCLUDEPATH += src src/json src/qt src/qt/plugins/mrichtexteditor
 QT += core gui widgets network printsupport
 DEFINES += ENABLE_WALLET
--- src/main.h
+++ src/main.h
@@ -86,7 +86,7 @@ static const int64_t nDrift = 5 * 60;
 /** Future drift params */
 inline int64_t FutureDrift(int64_t nTime) { return nTime + nDrift; }
 /** Velocity Factor handling toggle */
-inline bool FACTOR_TOGGLE(int nHeight) { return TestNet() || nHeight > 750000; } // Checks take place after block 750000
+inline bool FACTOR_TOGGLE(int nHeight) { return TestNet() || nHeight > 1000000; } // Checks take place after block 750000   - 1mil for beta -
 /** "reject" message codes **/
 static const unsigned char REJECT_INVALID = 0x10;
 
