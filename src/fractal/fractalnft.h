52506559615baa732de9d889df007c412474d27b
SaltineChips <rubixblockchain@gmail.com>
2021-11-13 07:30:30 -0800

    Commit 1.1.7.11
    -Re-implemented the fractal system
    
    -Enabled local storage of NFTs

--- src/fractal/fractalnft.h
+++ src/fractal/fractalnft.h
@@ -6,13 +6,13 @@
 #define FRACTALNFT_H
 
 #include <string>
-#include <vector>
 
-extern int n;
-extern std::string nftOut_String;
-extern bool iLOAD;
-extern bool load_image(std::vector<unsigned char>& image, const std::string& filename, int& x, int& y);
+extern std::string NFTdata;
+extern bool NFT_run;
 
-void NFTparse(std::string filename);
+void NFTrender(std::string input_mydata, std::string input_alias, std::string render_alias, int contract_type);
+void NFTprintRENDER(unsigned char NFTdata[], int w, int h, int channels, std::string passed_alias, int contract_type);
+void NFTparse(std::string image_to_deCode);
+void NFTenCode(std::string input_thatdata);
 
 #endif // FRACTALNFT_H
